47e428b30d00a5ef0b44e40b7a4997be855cd7d5
Bendik Witzoe <bendikw@gmail.com>
2017-11-02 15:09:10 +0100

    gogogo

--- src/som/Node.java
+++ src/som/Node.java
@@ -3,14 +3,11 @@ package som;
 public class Node {
 	
 	private double[] weights;
-	private double[] pos;
 	private Object label;
 	
-	public Node(double[] weights, double xPos, double yPos) {
+	public Node(double[] weights) {
 		this.weights = weights;
-		pos = new double[2];
-		this.pos[0] = xPos;
-		this.pos[1] = yPos;
+
 	}
 	
 	public double[] getWeights() {
@@ -21,17 +18,6 @@ public class Node {
 		this.weights = weights;
 	}
 	
-	public double[] getPos() {
-		return this.pos;
-	}
-	
-	public double getXPos() {
-		return this.pos[0];
-	}
-	
-	public double getYPos() {
-		return this.pos[1];
-	}
 	
 	public void setLabel(Object label) {
 		this.label = label;
