3fee78a1cab816db3e9968e1b0ca90abdc58309c
Bendik Witzoe <bendikw@gmail.com>
2017-11-02 10:04:24 +0100

    tsp

--- src/som/Tools.java
+++ src/som/Tools.java
@@ -1,5 +1,11 @@
 package som;
 
 public class Tools {
-
+	static float getEuclidian(int[] v1, int[] v2) {
+		float res = 0;
+		for (int i = 0; i < v2.length; i++) {
+			res+= Math.sqrt((v1[i]-v2[i])^2);
+		}
+		return res;
+	}
 }
